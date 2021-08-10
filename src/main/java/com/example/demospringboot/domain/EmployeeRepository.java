9601e406949fb69fa1ecb8bf4031a911edf3ef19
OKaluzny <kaluzny.oleg@gmail.com>
2021-08-10 14:20:20 +0300

    Added new entity Address

--- src/main/java/com/example/demospringboot/domain/EmployeeRepository.java
+++ src/main/java/com/example/demospringboot/domain/EmployeeRepository.java
@@ -1,12 +1,10 @@
 package com.example.demospringboot.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     List<Employee> findByName(String name);
-
 }
