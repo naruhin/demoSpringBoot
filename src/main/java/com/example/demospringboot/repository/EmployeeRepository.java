7996a121a1a5803554f18eb97bb73a18c986b6aa
naruhin <dimonkoko1@gmail.com>
2021-08-14 15:06:04 +0300

    new repository methods

---
+++ src/main/java/com/example/demospringboot/repository/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.example.demospringboot.repository;
+
+import com.example.demospringboot.domain.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List <Employee> findAllByIsDeletedIsFalse();
+
+    List <Employee> getAllByIsDeletedIsFalse();
+
+    Employee findByIdAndIsDeletedIsFalse(Long id);
+}
