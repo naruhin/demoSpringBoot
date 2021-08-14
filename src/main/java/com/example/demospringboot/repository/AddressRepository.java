4f3a32e8dc84f2dd43052c401edfd0a3f4f1e576
naruhin <dimonkoko1@gmail.com>
2021-08-14 15:06:43 +0300

    new repository methods

--- src/main/java/com/example/demospringboot/repository/AddressRepository.java
+++ src/main/java/com/example/demospringboot/repository/AddressRepository.java
@@ -6,5 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
-    List<Address> findByCity(String city);
+    List <Address> findAllByDeletedIsFalse();
+
+    List <Address> getAllByDeletedIsFalse();
+
+    Address findByIdAndDeletedIsFalse(Long id);
 }
