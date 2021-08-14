160c6f00a22e114ea4affa5c49193684670dc45a
naruhin <dimonkoko1@gmail.com>
2021-08-14 16:15:40 +0300

    remove useless methods

--- src/main/java/com/example/demospringboot/DemoSpringBootApplication.java
+++ src/main/java/com/example/demospringboot/DemoSpringBootApplication.java
@@ -1,41 +1,15 @@
 package com.example.demospringboot;
 
-import com.example.demospringboot.domain.Address;
-import com.example.demospringboot.domain.Employee;
-import com.example.demospringboot.repository.AddressRepository;
-import com.example.demospringboot.repository.EmployeeRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DemoSpringBootApplication {
 
     public static void main(String[] args) {
 
         SpringApplication.run(DemoSpringBootApplication.class, args);
-
-
     }
-
-    @Bean
-    public CommandLineRunner mappingDemo(AddressRepository addressRepository, EmployeeRepository employeeRepository) {
-        return args -> {
-
-            // create a new book
-            Address address = new Address("Tokio","Ukraine","000");
-
-            // save the book
-
-            addressRepository.save(address);
-
-            employeeRepository.save(new Employee("Sergey","asfsaf@gmail",address));
-            employeeRepository.save(new Employee("gena","denchik@gmail",address));
-            employeeRepository.save(new Employee("Sergey","рябчик@gmail",address));
-
-            // create and save new pages
-        };
-    }
-
 }
